33becd684096d99ca933b8c6d2fec0d694c32128
ssc <cong@parlayz.com>
2011-11-04 22:46:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharewell::Application.config.secret_token = '8f85b29c9386f4875fe815d9e8df8506185703c95f5aa659bb34fa3f9af7cd0e71c32f289911689a0b3fdff03aaaf38260f30ebba568eb933ae4273542d0ab84'
